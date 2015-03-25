8d14056a53e7e862c029389a561f59cfaf18a259
crutchcorn <crutchcorn@gmail.com>
2015-03-25 01:21:34 -0700

    Well, I fixed this little puppy. Even made it so that it can run in ash without bash!!! Crazy!!! This means that this guy will be leaving us soon

--- pool/config_files/declare.sh
+++ pool/config_files/declare.sh
@@ -1,8 +1,11 @@
-export NumberOfVMs=`cat /variables/NumberOfVMs`
-if [ -z "$2" ]; then
-	:
-else
-	NumberOfVMs=$2
-fi
-declare $1_$(printf %04g $NumberOfVMs)=$1
-echo $1_$(printf %04g $NumberOfVMs) > /variables/$1_$(printf %04g $NumberOfVMs)
+#!/bin/bash
+
+# filename variablename datainfo $NumberOfVMs
+# bash /cdrom/pool/config_files/declare.sh VirtualImageURL $VirtualImageURL $NumberOfVMs
+# Fun fact, this doesn't need bash to work... Which means this is going to leave us soon
+
+NumberOfVMs=$3
+variableName="$1_$(printf %04g $NumberOfVMs)"
+variableVal="$2"
+eval ${variableName}=`echo -ne \""${variableVal}"\"`
+echo $variableVal > /variables/${variableName}
